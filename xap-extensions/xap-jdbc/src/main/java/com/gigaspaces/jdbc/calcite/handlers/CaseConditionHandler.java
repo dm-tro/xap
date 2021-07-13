9f323a9bd43af2b677292915ce2e4ee0168b9396
Sagiv Michael <sagiv.michael@gigaspaces.com>
2021-07-13 08:50:58 +0300

    remove todos

--- xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/calcite/handlers/CaseConditionHandler.java
+++ xap-extensions/xap-jdbc/src/main/java/com/gigaspaces/jdbc/calcite/handlers/CaseConditionHandler.java
@@ -119,7 +119,7 @@ public class CaseConditionHandler extends RexShuttle {
             case CAST:
                 return handleTwoOperandsCall(getNode((RexLocalRef) ((RexCall) leftOp).getOperands().get(0)), rightOp,
                         sqlKind, isNot);//return from recursion
-            case DYNAMIC_PARAM://TODO: @sagiv needed? add test and check
+            case DYNAMIC_PARAM:
                 value = queryExecutor.getPreparedValues()[((RexDynamicParam) leftOp).getIndex()];
                 break;
             case ROW_NUMBER://TODO: @sagiv needed? add test and check
