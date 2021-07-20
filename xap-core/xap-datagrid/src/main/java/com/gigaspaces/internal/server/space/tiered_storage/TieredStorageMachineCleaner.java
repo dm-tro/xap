9d9a19714d9984eb30047f5eb8f46a0eb50a36f0
efratkimchi <efrat.kimchi@gigaspaces.com>
2021-07-20 09:12:21 +0300

    distribute pu between agents

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/tiered_storage/TieredStorageMachineCleaner.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/tiered_storage/TieredStorageMachineCleaner.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class TieredStorageMachineCleaner {
         private static Logger logger = LoggerFactory.getLogger(TieredStorageMachineCleaner.class);
 
+
     public static void deleteTieredStorageData(String spaceName) {
         if (logger.isDebugEnabled()){
             logger.debug("Trying to delete db of space {}", spaceName);
@@ -20,20 +21,18 @@ public class TieredStorageMachineCleaner {
         Path path = SystemLocations.singleton().work("tiered-storage/" + spaceName);
         File folder = path.toFile();
 
-        List<File> files = Arrays.asList(folder.listFiles());
-        logger.info("Is files is empty???"  + files.isEmpty());
-        if (files == null  || files.isEmpty()) {
+        File[] files = folder.listFiles();
+        if (files == null) {
             if (logger.isDebugEnabled()){
                 logger.debug("Did not find db of space {} ", spaceName);
             }
         } else {
-            files.add(folder);
             for (final File file : files) {
                 if (!file.delete()) {
                     logger.warn("Can't remove " + file.getAbsolutePath());
                 }
             }
-           // folder.delete();
+            folder.delete();
             logger.info("Successfully deleted db of space {} in path {}", spaceName, folder.getAbsolutePath());
         }
     }
